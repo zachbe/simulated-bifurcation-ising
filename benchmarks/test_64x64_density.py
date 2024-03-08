918b1b8f9c43724eacd4e5e97fab3395d2caea93
zbelateche <zachbela@alumni.stanford.edu>
2024-03-08 07:36:00 +0000

    Added a better test script

--- benchmarks/test_64x64_density.py
+++ benchmarks/test_64x64_density.py
@@ -2,6 +2,7 @@ import torch
 import copy
 import time
 import sys
+import numpy as np
 
 from simulated_bifurcation import ConvergenceWarning, reset_env, set_env
 from simulated_bifurcation.core import Ising
@@ -15,59 +16,74 @@ from simulated_bifurcation.optimizer import (
 # success probability, defined as the number of trials that
 # achieve 95% as good of a hamiltonian as the reference.
 
-# TODO: Add support for density sweeping
-
-if (len(sys.argv) != 3):
+if (len(sys.argv) != 4):
     print("should be run as:") 
-    print("sudo python3 test_64x64_density.py [num_tests] [num_trials]")
+    print("sudo python3 test_64x64_density.py [num_tests] [num_trials] [num_density_sweep_points]")
     sys.exit() 
 
 num_tests = int(sys.argv[1])
 num_trials = int(sys.argv[2])
+num_den_sweep = int(sys.argv[3])
 
-#Run num_tests J matrixes.
-for tests in range(num_tests):
+density_points = np.linspace(0, 0.9, num_den_sweep)
+f = open("data.csv", "w+")
 
-    # Pick random integer couplings, and symmetrize.
-    J = torch.randint(-7, 8, (63,63), dtype=torch.float32)
-    J = torch.round((J + J.t()) / 2)
-    h = torch.randint(-7, 8, (63,), dtype=torch.float32)
-    ising = Ising(J, h, use_fpga = True, digital_ising_size=64)
+for sparsity in density_points:
+    #Run num_tests J matrixes.
+    for tests in range(num_tests):
+    
+        # Pick random integer couplings.
+        J = torch.randint(-7, 8, (63,63), dtype=torch.float32)
 
-    # Use the simulated bifurcation algorithm to get a baseline solution.
-    ising.minimize(
-        1,
-        100000,
-        False,
-        False,
-        False,
-        use_window=False,
-        sampling_period=50,
-        convergence_threshold=50,
-        use_fpga = False
-    )
-    expected_data = copy.deepcopy(ising.computed_spins)
-    sim_energy = ising.get_energy()
-    print(sim_energy)
+        # Sparsify J, then symmetrize
+        s = torch.empty((63, 63))
+        torch.nn.init.sparse_(s, sparsity = sparsity)
+        s = torch.ceil(torch.abs(s))
+        J = torch.mul(J, s)
+        J = torch.round((J + J.t()) / 2)
 
-    # Run num_trials trials.
-    for trials in range(num_trials):
+        h = torch.randint(-7, 8, (63,), dtype=torch.float32)
+        ising = Ising(J, h, use_fpga = True, digital_ising_size=64)
+    
+        # Use the simulated bifurcation algorithm to get a baseline solution.
         ising.minimize(
             1,
-            10000,
+            100000,
             False,
             False,
             False,
             use_window=False,
             sampling_period=50,
             convergence_threshold=50,
-            use_fpga = True,
-            time_ms = 100,
-            shuffle_spins = True
+            use_fpga = False
         )
-        fpga_energy = ising.get_energy()
-        print(fpga_energy)
-        #if (fpga_energy <= 0.90*sim_energy):
-        #    print(".", end="")
-        #else:
-        #    print("X", end="")
+        expected_data = copy.deepcopy(ising.computed_spins)
+        sim_energy = ising.get_energy()
+        
+        f.write(str(sparsity)+",")
+        f.write(str(sim_energy[0].item())+",")
+    
+        # Run num_trials trials.
+        # We can't use agents here, because we need to
+        # re-shuffle each time.
+        for trial in range(num_trials):
+            ising.minimize(
+                1,
+                10000,
+                False,
+                False,
+                False,
+                use_window=False,
+                sampling_period=50,
+                convergence_threshold=50,
+                use_fpga = True,
+                time_ms = 100,
+                shuffle_spins = True
+            )
+            fpga_energy = ising.get_energy()
+            print(".")
+            f.write(str(fpga_energy[0].item())+",")
+
+        f.write("\n")
+
+
