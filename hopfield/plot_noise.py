4bac0e22454579f4cd3a0bc2965c9fc93268c939
zbelateche <zachbela@stanford.edu>
2024-04-08 23:14:18 -0700

    noise plot progress

--- hopfield/plot_noise.py
+++ hopfield/plot_noise.py
@@ -6,12 +6,15 @@ import numpy as np
 import statistics as stat
 
 # DIMPLE params
-noises = [  2,  4,   8,  10]
-cycles = [50, 100, 150, 200]
-num_tests = 5
+noises = [  2,  4,  6,  8, 10,  12,  14,  16,  18, 20 ]
+cycles = [  0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200 ]
+num_tests = 10
 
 # Parse data from CSV
 data = [[[],[]] for _ in noises]
+avg  = [[[],[]] for _ in noises]
+low  = [[] for _ in noises]
+high = [[] for _ in noises]
 
 noise_index = 0
 cycle_index = 0
@@ -22,28 +25,37 @@ with open('data_noise.csv', newline = "\n") as f:
             noise_index += 1
             cycle_index = 0
         else:
-            for score in row:
-                if score != "":
-                    data[noise_index][0].append(int(float(score)))
-                    data[noise_index][1].append(cycles[cycle_index])
+            if cycles[cycle_index] != 0:
+                total = 0
+                for score in row:
+                    if score != "":
+                        data[noise_index][0].append(1 - float(score)/64)
+                        data[noise_index][1].append(cycles[cycle_index] * 4)
+                        total += 1 - float(score)/64
+                avg_val = total/num_tests
+                avg[noise_index][0].append(avg_val)
+                avg[noise_index][1].append(cycles[cycle_index] * 4)
+                std = stat.pstdev(data[noise_index][0])
+                low[noise_index].append(avg_val - std)
+                high[noise_index].append(avg_val + std)
             cycle_index += 1
-
-#print(data)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-ax.hlines(0, 0, 200, color = "black", linestyles="dashed")
+ax.hlines(1, 20, 800, color = "black", linestyles="dashed")
 
 color = iter(plt.cm.gist_rainbow(np.linspace(0, 1, len(noises))))
 
 for i in range(len(data)):
     c = next(color)
-    ax.scatter(data[i][1], data[i][0], color=c, s = 100-(30*i))
+    ax.scatter(data[i][1], data[i][0], color=c, s = 20)
+    ax.plot(avg[i][1], avg[i][0], color=c)
+#    ax.fill_between(avg[i][1], low[i], high[i], fc = c, ec = c, lw = 1, linestyles = "dashed", alpha = 0.5)
 
 #ax.legend()
 
 ax.set_ylabel("Accuracy")
-ax.set_xlabel("Settling Time")
+ax.set_xlabel("Settling Time (ns)")
 
 plt.show()
