70ca901842b981e986f26c3f11438250c1209139
zbelateche <zachbela@alumni.stanford.edu>
2024-04-30 03:11:56 +0000

    Cleaned up hopfield directory

--- hopfield/hopfield_settle.py
+++ hopfield/hopfield_settle.py
@@ -9,49 +9,49 @@ from simulated_bifurcation.optimizer import (
     SimulatedBifurcationOptimizer,
 )
 
-# Image 1: Smiley
-smiley = np.array([[-1, -1, -1, -1, -1, -1, -1, -1],
-                   [-1, -1,  1, -1, -1,  1, -1, -1],
-                   [-1, -1,  1, -1, -1,  1, -1, -1],
-                   [-1, -1, -1, -1, -1, -1, -1, -1],
-                   [-1,  1, -1, -1, -1, -1,  1, -1],
-                   [-1,  1, -1, -1, -1, -1,  1, -1],
-                   [-1, -1,  1,  1,  1,  1, -1, -1],
-                   [-1, -1, -1, -1, -1, -1, -1, -1]])
+# Image 1: Checker
+checker   = np.array([[ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [-1, -1, -1, -1,  1,  1,  1,  1],
+                      [-1, -1, -1, -1,  1,  1,  1,  1],
+                      [-1, -1, -1, -1,  1,  1,  1,  1],
+                      [-1, -1, -1, -1,  1,  1,  1,  1]])
 
-# Image 2: Check
-check  = np.array([[-1, -1, -1, -1, -1, -1, -1, -1],
-                   [-1, -1, -1, -1, -1, -1,  1, -1],
-                   [-1, -1, -1, -1, -1,  1,  1, -1],
-                   [-1,  1, -1, -1, -1,  1, -1, -1],
-                   [-1,  1,  1, -1,  1,  1, -1, -1],
-                   [-1, -1,  1,  1,  1, -1, -1, -1],
-                   [-1, -1, -1,  1, -1, -1, -1, -1],
-                   [-1, -1, -1, -1, -1, -1, -1, -1]])
+# Image 2: Box
+box       = np.array([[ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1],
+                      [ 1,  1,  1,  1, -1, -1, -1, -1]])
 
-# Make a noisy version of smiley
-noise = np.random.randint(10, size=(8,8))
-smiley_noise = [[-s if n == 0 else s for s, n in zip(sm, no)] for sm, no in zip(smiley, noise)]
-smiley_noise = np.array(smiley_noise)
-smiley_noise_1d = smiley_noise.reshape(64)
+# Make a noisy version of checker
+noise = np.random.randint(4, size=(8,8))
+checker_noise = [[-s if n == 0 else s for s, n in zip(sm, no)] for sm, no in zip(checker, noise)]
+checker_noise = np.array(checker_noise)
+checker_noise_1d = checker_noise.reshape(64)
 
 # Compute the weights for the hopfield network
-smiley_1d = smiley.reshape(64)
-check_1d  = check.reshape(64)
+checker_1d = checker.reshape(64)
+box_1d  = box.reshape(64)
 weights = np.zeros((64,64))
 
 # Set weights via Hebbian learning
 for i in range(64):
     for j in range(64):
-        weights[i][j] = ((smiley_1d[i] * smiley_1d[j]) + (check_1d[i] * check_1d[j]))
+        weights[i][j] = ((checker_1d[i] * checker_1d[j]) + (box_1d[i] * box_1d[j]))
 
 ising = Ising(weights, digital_ising_size = 64, use_fpga = True)
 
 # DIMPLE params
-cycles = [100, 200, 300]
+cycles = [50, 100, 150, 200]
 
-# Use DIMPLE to recall smiley in steps
-smilies_out = [smiley_noise]
+# Use DIMPLE to recall checker in steps
+checkers_out = [checker_noise]
 
 for i in range(len(cycles)):
     ising.minimize(
@@ -60,17 +60,17 @@ for i in range(len(cycles)):
         use_fpga = True,
         shuffle_spins = False,
         cycles = cycles[i],
-        initial_spins = smiley_noise_1d,
+        initial_spins = checker_noise_1d,
         reprogram_J = (i == 0)
     )
-    smilies_out.append(ising.computed_spins.numpy().reshape((8,8)))
+    checkers_out.append(ising.computed_spins.numpy().reshape((8,8)))
 
 cycles.insert(0,0)
 
 # Show images
 for i in range(len(cycles)):
     ax = plt.subplot(1, len(cycles), i+1)
-    ax.imshow(smilies_out[i], interpolation='nearest')
+    ax.imshow(checkers_out[i], interpolation='nearest')
     sec = cycles[i]*4
     ax.title.set_text(f"{sec} ns")
     ax.axis("off")
