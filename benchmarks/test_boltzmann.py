92acb6c4497da27bd12bebc9ad5f281a740f8005
zbelateche <zachbela@alumni.stanford.edu>
2024-05-14 23:50:19 +0000

    1D ising

--- benchmarks/test_boltzmann.py
+++ benchmarks/test_boltzmann.py
@@ -20,12 +20,14 @@ num_trials = int(sys.argv[1])
 
 f = open("data_boltzmann.csv", "w+")
 
-J = torch.randint(-7, 8, (63,63), dtype=torch.float32)
-h = torch.randint(-7, 8, (63,), dtype=torch.float32)
-ising = Ising(J, h, use_fpga = True, digital_ising_size=64)
+J = torch.zeros((63,63), dtype=torch.float32)
+h = torch.ones((63,), dtype=torch.float32)
 
-torch.save(J, "J.pt")
-torch.save(h, "h.pt")
+for i in range(0, 62):
+    J[i][i+1] = -7
+    J[i+1][i] = -7
+
+ising = Ising(J, h, use_fpga = True, digital_ising_size=64)
     
 # Use the simulated bifurcation algorithm to get a baseline solution.
 ising.minimize(
