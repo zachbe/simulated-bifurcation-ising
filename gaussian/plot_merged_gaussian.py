db400063edbc48430c9d12b691f59fd207dda89e
zachbe <zachbela@alumni.stanford.edu>
2024-07-22 15:10:42 -0700

    Known-good matrix works

--- gaussian/plot_merged_gaussian.py
+++ gaussian/plot_merged_gaussian.py
@@ -7,23 +7,33 @@ import statistics as stat
 import math
 
 samples = np.load("gaussian_samples_rand.npy")
+J = np.load("gaussian_J_rand.npy")
+print(-J)
+print(np.linalg.inv(-J))
 
-a = []
-b = []
+num_units = 2
+total_spins = 64
+per = int(total_spins / num_units)
+
+gaussian = [[] for _ in range(num_units)]
 
 for trial in range(len(samples)):
-    a_sam = int(np.sum(samples[trial][0:32]))
-    b_sam = int(np.sum(samples[trial][32:63]))
-    a.append(a_sam)
-    b.append(b_sam)
-
+    for unit in range(num_units):
+        sam = int(np.sum(samples[trial][per * unit : per * (unit+1)]))
+        gaussian[unit].append(sam)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-ax.hist(a, bins=100)
-ax.hist(b, bins=100)
+for unit in gaussian:
+    ax.hist(unit, bins=100)
 
-print(np.cov(a,b))
+cov = [[0 for c in range(num_units)] for r in range(num_units)]
+
+for a_i in range(num_units):
+    for b_i in range(num_units):
+        cov[a_i][b_i] = float(np.cov(gaussian[a_i], gaussian[b_i])[0][1])
+
+print(cov)
 
 plt.show()
