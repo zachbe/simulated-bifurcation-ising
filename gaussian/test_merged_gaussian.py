fc06106d94f485c62bfcb2afb514e40bb618756f
zbelateche <zachbela@alumni.stanford.edu>
2024-07-22 22:37:07 +0000

    Trying a known-good 4x4

--- gaussian/test_merged_gaussian.py
+++ gaussian/test_merged_gaussian.py
@@ -32,7 +32,8 @@ f = open("data_gaussian_rand.csv", "w+")
 ## h = torch.from_numpy(h).float()
 
 # Pick random positive integer couplings
-J = torch.randint(-3, 0, (4,4), dtype=torch.float32)
+#J = torch.randint(-3, 0, (4,4), dtype=torch.float32)
+J = torch.from_numpy(np.array([[-2,-1,-1,-1],[-1,-2,-1,-1],[-1,-1,-2,-1],[-1,-1,-1,-2]])).float()
 J = torch.round((J + J.t()) / 2)
 
 print(-J)
