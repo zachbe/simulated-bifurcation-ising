4384ea8ab43d0798dc11616740962e5f48154b8e
zbelateche <zachbela@stanford.edu>
2024-07-26 14:45:39 -0700

    WIP QUBO formulation

---
+++ qubo_gaussian/plot_merged_gaussian.py
@@ -0,0 +1,63 @@
+
+import sys
+import csv
+#import matplotlib.pyplot as plt
+import numpy as np
+import statistics as stat
+import math
+
+
+# Read in data and print expected values
+samples = np.load("gaussian_samples_rand.npy")
+J = np.load("gaussian_J_rand.npy")
+print("Input matrix")
+print(-J)
+inv = np.linalg.inv(-J)
+print("Target inverse")
+print(inv/inv[0][0])
+
+#h = np.load("gaussian_h_rand.npy")
+#print("Input vector")
+#print(-h)
+#sol = inv @ (-h)
+#print("Linear system solution")
+#print(sol/sol[0])
+
+print("--------------------------")
+
+# Paramters for data collation
+num_units = 3
+total_spins = 60
+per = int(total_spins / num_units)
+
+# Collate data into buckets
+gaussian = [[] for _ in range(num_units)]
+for trial in range(len(samples)):
+    for unit in range(num_units):
+        sam_qubo = samples[trial]
+        sam_qubo[sam_qubo == -1] = 0
+        sam = int(np.sum(sam_qubo[per * unit : per * (unit+1)]))
+        gaussian[unit].append(sam)
+
+# Calculate mean and covariance
+cov = [[0 for c in range(num_units)] for r in range(num_units)]
+for a_i in range(num_units):
+    for b_i in range(num_units):
+        cov[a_i][b_i] = float(np.cov(gaussian[a_i], gaussian[b_i])[0][1])
+print("Device inverse")
+print(np.array(cov)/cov[0][0])
+
+#mean = [0 for _ in range(num_units)]
+#for a in range(num_units):
+#    mean[a] = np.sum(gaussian[a])/per
+#print("Device linear system solution")
+#print(mean/mean[0])
+
+## Plot hisograms
+#fig = plt.figure()
+#ax = fig.add_subplot(1,1,1)
+#
+#for unit in gaussian:
+#    ax.hist(unit, bins=100)
+#
+#plt.show()
---
+++ qubo_gaussian/test_merged_gaussian.py
@@ -0,0 +1,92 @@
+import torch
+import copy
+import time
+import sys
+import numpy as np
+
+from simulated_bifurcation import ConvergenceWarning, reset_env, set_env
+from simulated_bifurcation.core import Ising
+from simulated_bifurcation.optimizer import (
+    SimulatedBifurcationEngine,
+    SimulatedBifurcationOptimizer,
+)
+
+if (len(sys.argv) != 2):
+    print("should be run as:") 
+    print("sudo python3 test_gaussian.py [num_samples]")
+    sys.exit() 
+
+num_trials = int(sys.argv[1])
+
+f = open("data_gaussian_rand.csv", "w+")
+
+# Pick known-good couplings
+J = torch.tensor([[-2,0,-1],[0,-1,0],[-1,0,-2]]).float()
+
+# Ising -> qubo:
+# h_i = 2 * sum of q_ij for all j
+h = torch.tensor([-6, -2, -6]).float()
+
+print(-J)
+print(np.linalg.inv(-J))
+
+with open("gaussian_J_rand.npy", "wb+") as bf:
+    np.save(bf, J.numpy())
+
+with open("gaussian_h_rand.npy", "wb+") as bf:
+    np.save(bf, h.numpy())
+
+# Convert to multibit representation
+J = np.kron(J, np.ones((21,21)))
+h = np.kron(h, np.ones(21))
+
+J = torch.from_numpy(J).float()
+h = torch.from_numpy(h).float()
+
+ising = Ising(J, h, use_fpga = True, digital_ising_size=64)
+    
+# Use the simulated bifurcation algorithm to get a baseline solution.
+ising.minimize(
+    1,
+    100000,
+    False,
+    False,
+    False,
+    use_window=False,
+    sampling_period=50,
+    convergence_threshold=50,
+    use_fpga = False
+)
+expected_data = copy.deepcopy(ising.computed_spins)
+sim_energy = ising.get_energy()
+
+f.write(str(sim_energy[0].item())+",")
+
+data = np.zeros((num_trials, 63))
+for trial in range(num_trials):
+    ising.minimize(
+        1,
+        10000,
+        False,
+        False,
+        False,
+        autoscale = False,
+        use_window=False,
+        sampling_period=50,
+        convergence_threshold=50,
+        use_fpga = True,
+        cycles = 100000,
+        shuffle_spins = False,
+        reprogram_J = (trial == 0),
+        counter_cutoff = 0,
+        counter_max = 1
+    )
+    fpga_energy = ising.get_energy()
+    print(f"{trial}")
+    snp = ising.computed_spins.numpy()
+    for i in range(len(snp)):
+        data[trial][i] = snp[i][0]
+    f.write(str(fpga_energy[0].item())+",")
+
+with open("gaussian_samples_rand.npy", "wb+") as bf:
+    np.save(bf, data)
